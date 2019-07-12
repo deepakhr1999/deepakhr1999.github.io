d63065b8ce6140128ce0c06929404e857cbb54de
unknown <deepakhr1999@gmail.com>
2019-07-12 14:28:55 +0530

    client side routing

--- js/params.js
+++ js/params.js
@@ -2,4 +2,24 @@ const resume_link = "insert google drive link here"
 
 
 
-document.getElementById("resume").setAttribute("href", resume_link)
+// document.getElementById("resume").setAttribute("href", resume_link)
+
+
+
+// routing
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "home.html"
+    })
+    .when("/interests", {
+        templateUrl : "interests.html"
+    })
+    .when("/contact",{
+        templateUrl: "contact.html"
+    })
+    .when("/project", {
+        templateUrl : "projects.html"
+    });
+});
--- mail.php
+++ mail.php
@@ -1,27 +1,25 @@
 <?php
-    $to = 'deepakhr1999@gmail.com';
-    $firstname = $_POST["fname"];
-    $email= $_POST["email"];
+    $recep = 'deepakhr1999@gmail.com';
+    $firstname = $_POST["name"];
+    $sender= $_POST["email"];
     $text= $_POST["message"];
-    $phone= $_POST["phone"];
-    
+    $subject = $_POST["subject"];
 
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers = 'MIME-Version: 1.0' . "\r\n"
+    ."From: " . $sender . "\r\n" 
+    .'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table style="width:100%">
         <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
+            <td>'.$firstname.'  '.$subject.'</td>
         </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
+        <tr><td>Email: '.$sender.'</td></tr>
         <tr><td>Text: '.$text.'</td></tr>
         
     </table>';
 
-    if (@mail($to, $email, $message, $headers))
+    if (mail($recep, $sender, $message, $headers))
     {
         echo 'The message has been sent.';
     }else{
